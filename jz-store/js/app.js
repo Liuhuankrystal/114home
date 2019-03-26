0d04ec47f21b925231a950f0e7c86b941054e816
Big-Cxian <346313606@qq.com>
2019-03-26 20:16:53 +0800

    配置

--- jz-store/js/app.js
+++ jz-store/js/app.js
@@ -1,6 +1,6 @@
 var timeoutSet = "60000",
 	  canSend = true,
-		pubilcUrl = "https://o2o.114yun.net/sysAdmin/",
+		pubilcUrl = "http://demo.jixinghai.com/xiaobaidaojia/public/index.php",
 		passWordRegex = /^(?![a-zA-z]+$)(?!\d+$)(?![!@#$%^&*]+$)(?![a-zA-z\d]+$)(?![a-zA-z!@#$%^&*]+$)(?![\d!@#$%^&*]+$)[a-zA-Z\d!@#$%^&*]+$/,
 		hanzi = /^[\u4e00-\u9fa5]+$/,
 		telRegex = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/,
